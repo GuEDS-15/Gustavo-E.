03be79978170e4b8fa52679efaba058ca2b14dae
GuEDS-15 <gustavorei60@gmail.com>
2024-08-30 14:27:29 -0300

    Add files via upload

---
+++ exercicios2.js
@@ -0,0 +1,11 @@
+//Crie uma variavel que receba e armazene a sua idade no tipo number
+let idade = 15
+//Crie uma outra variavel que receba o valor da primeira variavel, mas com o seu tipo alterado para string
+let idade2 =  idade.toString()
+3. //Imprime duas mensagens no terminal
+//A primeira deve mostra o valor e o tipo da primeira variavel
+//A segunda deve mostrar o valor e o tipo da segunda variavel
+
+
+console.log(idade, typeof idade)
+console.log(idade, typeof idade2)
