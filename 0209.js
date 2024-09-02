5fbbbbc7f2cb4244f4e01da8ff689b70d064f38c
GuEDS-15 <gustavorei60@gmail.com>
2024-09-02 16:52:27 -0300

    Add files via upload

---
+++ 0209.js
@@ -0,0 +1,73 @@
+//EXECERCICIO 1
+let modulo = 80 % 7
+console.log(modulo)
+let resultado = 100
+resultado += 20
+resultado -= 10
+resultado *= 58
+
+let val = 3
+let val2 = 4
+console.log(val + val2)
+
+let val = 3
+let val2 = 5
+console.log(val * val2)
+console.log(val / val2)
+
+let val = 5
+let val2 = 4
+console.log(val * val2)
+
+
+let val = 234
+let val2 = 5
+console.log(val / val2)
+console.log(val % val2)
+ 
+let val1 = 4 - 5
+let var2 = -1
+console.log(val1 * var2)
+
+//exercicio 2
+//1
+let val1 = 5 === 5
+console.log(val1)
+//2
+let val2 = 6 === 2
+console.log(val2)
+//3 
+let val3 = 5 >= 4
+ console.log(val3)
+//4
+ let val4 = 5 < 4
+ console.log(val4)
+
+//EXERCICIO 3
+//1
+
+let var5 =(true && true)
+console.log(var5)
+
+let a = true 
+let b = false
+let c = true
+console.log(a && b)
+console.log(b && c)
+console.log(a && c)
+console.log(a && b && c)
+
+let a1 = true 
+let b1 = false
+let c1 = true
+console.log(a || b)
+console.log(b || c)
+console.log(a || c)
+console.log(a || b || c)
+
+let nome = "Gustavo"
+let idade = 15
+let maiorDeIdade = (idade >= 18)
+let Idadeem2050 = (idade + 26)
+console.log(50 - 15)
+console.log("meu nome eh", nome, "minha idade eh", idade, "é maior de idade", maiorDeIdade)
